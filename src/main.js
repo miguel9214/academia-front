462e5032178d6e02920c46c1682b036f5167aab3
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2024-01-06 00:33:07 -0500

    font y axios agregados

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'axios'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
--- src/views/StudentEdit.vue
+++ src/views/StudentEdit.vue
@@ -1,5 +1,3 @@
 <template>
-    <div>
-        <h1>Student Edit</h1>
-    </div>
+
 </template>
